14d7dcaf0a757c07d0a045ef9f718670c4c31142
Sebastian Will <sebastian.will@tik.uni-stuttgart.de>
2019-07-10 13:02:34 +0200

    Added ScaleTokenToGridSize snippet

---
+++ snippets/scaleTokensToGridSize.js
@@ -0,0 +1,46 @@
+let scaleTokensToGridSize = () => {
+    Hooks.on('preCreateToken', (sceneId, token) => {
+
+        // get the scene
+        let scene = game.scenes.get(sceneId);
+        console.log(scene);
+
+        // if the scenes grid distance is measured in feet, we
+        // will scale the token according to the scene itself
+        if (scene.data.gridUnits.indexOf('ft') !== -1) {
+            // get the grid size
+            let gridSize = scene.data.gridDistance;
+
+            // grid units at 5ft are:
+            let sizes = new Map();
+            sizes.set('tiny', 0.5);
+            sizes.set('sm', 1);
+            sizes.set('med', 1);
+            sizes.set('lg', 2);
+            sizes.set('huge', 3);
+            sizes.set('grg', 4);
+
+            // get the actor size
+            let actor = game.actors.get(token.actorId);
+            let actorSize = actor.data.data.traits.size;
+
+            // let's see if we have info about the grid-units a create
+            // of this size should occupy on a 5ft grid
+            if (sizes.has(actorSize.value)) {
+
+                let targetSize = sizes.get(actorSize.value);
+                let scale = 5 / gridSize;
+
+                // put the token width/height for a map with a 5ft grid
+                if (token.width !== targetSize || token.height !== targetSize) {
+                    token.width = targetSize;
+                    token.height = targetSize;
+                }
+
+                // then scale the token if the map has a grid != 5ft
+                // scale is 1 for a map with 5ft grid, so it won't be changed
+                token.scale = scale;
+            }
+        }
+    });
+};
